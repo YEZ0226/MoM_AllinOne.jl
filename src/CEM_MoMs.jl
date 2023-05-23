acdc73804b1a751653850618f949c3e7a0083e91
xyhe <1225385871@qq.com>
2023-05-24 00:52:50 +0800

    `examples` 文件夹中的脚本用例计算量较大，不再作为默认测试脚本。

--- src/CEM_MoMs.jl
+++ src/CEM_MoMs.jl
@@ -3,4 +3,17 @@ module CEM_MoMs
 using Reexport
 @reexport using MoM_Basics, MoM_Kernels, MoM_Visualizing
 
+export run_examples
+
+"""
+    run_examples(;targetdir = joinpath(@__DIR__, "../examples"))
+
+运行 `targetdir` 中的所有脚本。
+"""
+function run_examples(;targetdir = joinpath(@__DIR__, "../examples"))
+    files = readdir(targetdir)
+    map(f -> include(joinpath(targetdir, f)), files)
+    nothing
+end
+
 end # module CEM_MoMs
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,5 @@
-using Test#, Pkg
+using Test, Pkg
 
-files = readdir(joinpath(@__DIR__, "../examples"))
-map(f -> include(joinpath(@__DIR__, "../examples", f)), files)
-
-# for package in ["MoM_Basics", "MoM_Kernels", "MoM_Visualizing"]
-#     Pkg.test(package)
-# end
+for package in ["MoM_Basics", "MoM_Kernels", "MoM_Visualizing"]
+    Pkg.test(package)
+end
